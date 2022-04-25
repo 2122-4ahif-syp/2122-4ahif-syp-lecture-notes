d27862edc7a9189b0f5083d45cd545135903b887
stuetz <t.stuetz@htl-leonding.ac.at>
2022-04-25 19:14:07 +0200

    lecture 2022-04-15

---
+++ labs/karate-demo/src/test/java/at/htl/karatedemo/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl.karatedemo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
